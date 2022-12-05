91ec99cc243b0b638b5761f8a43659ea083bbfad
Antoine Cheynet <antoine@typedin.dev>
2022-12-05 15:29:45 +0100

    wip

--- tests/Api/CalendlyApiTest.php
+++ tests/Api/CalendlyApiTest.php
@@ -2,8 +2,19 @@
 
 namespace Typedin\LaravelCalendly\Tests\Api;
 
+use ArgumentCountError;
 use PHPUnit\Framework\TestCase;
+use Typedin\LaravelCalendly\Api\CalendlyApi;
 
 class CalendlyApiTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function it_throws_without_api_url_or_without_api_key(): void
+    {
+        $this->expectException(ArgumentCountError::class);
+
+        new CalendlyApi();
+    }
 }
