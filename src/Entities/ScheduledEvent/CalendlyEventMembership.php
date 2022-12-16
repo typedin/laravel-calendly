9be522e88cc2e0fe5ba1871b1ba67d18d9dc2416
typedin <typedin@users.noreply.github.com>
2022-12-16 17:11:11 +0000

    Fix styling

--- src/Entities/ScheduledEvent/CalendlyEventMembership.php
+++ src/Entities/ScheduledEvent/CalendlyEventMembership.php
@@ -24,7 +24,7 @@ class CalendlyEventMembership extends CalendlyUser
     public function __construct(array $args)
     {
         $this->keys()->each(function ($key) use ($args) {
-            if (!array_key_exists($key, $args)) {
+            if (! array_key_exists($key, $args)) {
                 CalendlyOrganizationMembershipException::nestedKeyNotFound($key);
             }
         });
