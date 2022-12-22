187288940db9216a88f9a5993227007c8bd7c525
Antoine Cheynet <antoine@typedin.dev>
2022-12-23 00:16:23 +0100

    wip

---
+++ src/Supports/Mapper.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Typedin\LaravelCalendly\Supports;
+
+use Illuminate\Support\Collection;
+
+class Mapper
+{
+    public const LOOKUP = [
+        'Data Compliance' => 'data_compliance',
+        'Event Types' => 'event_types',
+        'Organizations' => 'organizations',
+        'Routing Forms' => 'routing_forms',
+        'Scheduled Events' => 'scheduled_events',
+        'Scheduling Links' => 'scheduled_links',
+        'Users' => 'users',
+        'Webhooks' => 'webhook_subscriptions',
+    ];
+
+    /**
+     * @var Collection<TKey,TValue>
+     */
+    private Collection $paths;
+
+    public function __construct(private array $data, private string $tag)
+    {
+        $this->paths = collect($this->data['paths']);
+    }
+
+    public function handle(): Collection
+    {
+        return $this->endpoints()
+               ->map(fn ($value) => [
+                   'uri' => $value['uri'],
+                   'methods' => $this->getMethodsForEndpoint($value['uri']),
+               ]);
+    }
+
+    private function endpoints(): Collection
+    {
+        return $this->paths
+                ->keys()
+                ->filter(fn ($ke) => $this->findEndpointForTag($ke))
+                ->map(fn ($uri) => ['uri' => $uri]);
+    }
+
+    private function findEndpointForTag($key): bool
+    {
+        return explode('/', $key)[1] == self::LOOKUP[$this->tag];
+    }
+
+    private function getMethodsForEndpoint($uri): array
+    {
+        return collect($this->paths->get($uri))
+                ->filter(fn ($value, $key) => in_array($key, ['get', 'post', 'delete']))
+                ->all();
+    }
+}
---
+++ tests/Supports/MapperTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Typedin\LaravelCalendly\Tests\Supports;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Yaml;
+use Typedin\LaravelCalendly\Supports\Mapper;
+
+class MapperTest extends TestCase
+{
+    private Mapper $sut;
+
+    protected function setUp(): void
+    {
+        parent::setup();
+        $paths = Yaml::parse(file_get_contents(__DIR__.'/../../doc/openapi.yaml'));
+
+        $this->sut = (new Mapper($paths, 'Scheduled Events'));
+    }
+
+    /** @test */
+    public function it_collects_maps_rest_methods_to_uri(): void
+    {
+        $result = $this->sut->handle()->all();
+
+        $this->assertCount(5, $result);
+        $this->assertEquals('/scheduled_events/{uuid}/invitees', $result[0]['uri']);
+        $this->assertCount(1, $result[0]['methods']);
+        $this->assertTrue(in_array('get', array_keys($result[0]['methods'])));
+    }
+}
