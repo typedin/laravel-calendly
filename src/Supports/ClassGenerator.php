9da57482e72735f7090bab29b72a85b71c36653c
Antoine Cheynet <antoine@typedin.dev>
2022-12-24 15:41:39 +0100

    closed todo

--- src/Supports/ClassGenerator.php
+++ src/Supports/ClassGenerator.php
@@ -6,17 +6,29 @@ use Nette\PhpGenerator\ClassType;
 
 class ClassGenerator
 {
+    private ClassType $class;
+
+    private function __construct(private string $tag, private array $array)
+    {
+    }
+
     public static function generate(string $tag, array $array): ClassType
     {
-        $classname = implode(explode(' ', $tag)).'ApiClient';
-        $class = new ClassType($classname);
-        // TODO clean this
-        foreach ($array as $key => $value) {
-            foreach ($value['methods'] as $key => $fuck) {
-                $class->addMember(MethodGenerator::generate($key, $fuck));
-            }
-        }
+        $generator = new self($tag, $array);
+        $generator->class = new ClassType($generator->classname());
 
-        return $class;
+        collect($array)->each(fn ($value) => $generator->generateMethodsForUri($value['methods']));
+
+        return $generator->class;
+    }
+
+    private function classname(): string
+    {
+        return  implode(explode(' ', $this->tag)).'ApiClient';
+    }
+
+    private function generateMethodsForUri($methods): void
+    {
+        collect($methods)->each(fn ($method, $key) => $this->class->addMember(MethodGenerator::generate($key, $method)));
     }
 }
