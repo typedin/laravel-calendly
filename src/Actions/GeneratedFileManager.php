233084052f6bb2fbebde6131a2f414f18c20acfb
Antoine Cheynet <antoine@typedin.dev>
2022-12-24 16:33:19 +0100

    generate valid php files

--- src/Actions/GeneratedFileManager.php
+++ src/Actions/GeneratedFileManager.php
@@ -9,8 +9,10 @@ class GeneratedFileManager
     public static function writeAll(PhpNamespace $namespace, string $destination): void
     {
         foreach ($namespace->getClasses() as $class) {
+            $content = '<?php'.PHP_EOL."\n";
+            $content = $content.$class->__toString();
             // the @ suppresses the warning
-            $return_value = @file_put_contents($destination.$class->getName().'.php', $class->__toString());
+            $return_value = @file_put_contents($destination.$class->getName().'.php', $content);
             if (! $return_value) {
                 throw new \Exception(sprintf('Could not write file (%s) in folder: %s', $class->getName().'.php', $destination));
             }
