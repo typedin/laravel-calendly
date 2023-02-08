d60db2ebe7f60dd856ee3394c96064a6daf0e48b
typedin <typedin@users.noreply.github.com>
2023-02-08 20:46:37 +0000

    [create-pull-request] automated change

--- src/Http/Errors/ErrorResponse.php
+++ src/Http/Errors/ErrorResponse.php
@@ -6,13 +6,10 @@ use Illuminate\Http\JsonResponse;
 
 class ErrorResponse
 {
-    /** @var string */
     public string $title;
 
-    /** @var string */
     public string $message;
 
-    /** @var array */
     public array $details;
 
     public int $error_code;
