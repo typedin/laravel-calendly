07a78cb8cc293369c9198df0143522f0b02b3f42
typedin <antoine@typedin.dev>
2023-02-12 15:12:44 +0100

    wip

--- src/Supports/RouteGenerator.php
+++ src/Supports/RouteGenerator.php
@@ -7,7 +7,7 @@ use Typedin\LaravelCalendly\Supports\Configuration\ControllerGeneratorProvider;
 
 class RouteGenerator
 {
-    private function __construct(private  readonly EndpointMapper $mapper, private readonly string $path)
+    private function __construct(private readonly EndpointMapper $mapper, private readonly string $path)
     {
     }
 
@@ -15,11 +15,11 @@ class RouteGenerator
     {
         $generator = new RouteGenerator($mapper, $path);
 
-        return $generator->controllerProviderForPath()->flatMap(fn (ControllerGeneratorProvider $provider) => $generator->buildRoutes($provider)
+        return $generator->controllerProviderForCurrentPath()->flatMap(fn (ControllerGeneratorProvider $provider) => $generator->buildRoutes($provider)
         );
     }
 
-    private function controllerProviderForPath(): Collection
+    private function controllerProviderForCurrentPath(): Collection
     {
         return $this->mapper->controllerGeneratorProviders()->filter(fn (ControllerGeneratorProvider $provider) => in_array($this->path, array_keys($provider->endpoints)));
     }
@@ -27,6 +27,28 @@ class RouteGenerator
     private function buildRoutes($provider): Collection
     {
         return collect(array_keys($provider->endpoints[$this->path]))
-            ->map(fn ($method) => sprintf('Route::%s("%s", [%s::class, "show"])', $method, $this->path, $provider->controllerName()));
+            ->filter(fn ($method) => $method != 'parameters')
+            ->map(fn ($method) => sprintf('Route::%s("%s", [%s::class, "%s"])', $method, $this->path, $provider->controllerName(), $this->getControllerMethod($provider->endpoints, $method)));
+    }
+
+    private function getControllerMethod($endpoints, $method): string
+    {
+        $responses = $endpoints[$this->path][$method]['responses'];
+        if (in_array(200, array_keys($responses))) {
+            if (array_key_exists('collection', $responses[200]['content']['application/json']['schema']['properties'])) {
+                return 'index';
+            }
+            if (array_key_exists('resource', $responses[200]['content']['application/json']['schema']['properties'])) {
+                return 'show';
+            }
+        }
+        if (in_array(201, array_keys($responses))) {
+            return 'create';
+        }
+        if (in_array(204, array_keys($responses))) {
+            return 'destroy';
+        }
+
+        throw new \Exception('Could not determine a method for the path:'.$this->path, 1);
     }
 }
--- tests/Supports/RouteGeneratorTest.php
+++ tests/Supports/RouteGeneratorTest.php
@@ -16,11 +16,46 @@ class RouteGeneratorTest extends TestCase
         $this->mapper = new EndpointMapper(file_get_contents(__DIR__.'/../../doc/openapi.yaml'));
     }
 
+    /**
+     * @return array<TKey,TValue>
+     */
+    private function endpoints(mixed $filter): array
+    {
+        return $this->mapper->mapControllerNamesToEndpoints()->get($filter)->all();
+    }
+
     /** @test */
-    public function it_generate_a_route_for_a_single_method()
+    public function it_generates_a_route_for_a_single_show_method()
     {
         $path = '/users/{uuid}';
 
         $this->assertContains('Route::get("/users/{uuid}", [CalendlyUsersController::class, "show"])', RouteGenerator::fromPath($this->mapper, $path));
+    }
+
+    /** @test */
+    public function it_generates_two_routes_for_get_and_destroy()
+    {
+        $path = '/organizations/{org_uuid}/invitations/{uuid}';
+
+        $this->assertContains('Route::get("/organizations/{org_uuid}/invitations/{uuid}", [CalendlyOrganizationInvitationsController::class, "show"])', RouteGenerator::fromPath($this->mapper, $path));
+        $this->assertContains('Route::delete("/organizations/{org_uuid}/invitations/{uuid}", [CalendlyOrganizationInvitationsController::class, "destroy"])', RouteGenerator::fromPath($this->mapper, $path));
+    }
+
+    /** @test */
+    public function it_generates_index_method()
+    {
+        $path = '/organizations/{uuid}/invitations';
+
+        $this->assertContains('Route::get("/organizations/{uuid}/invitations", [CalendlyOrganizationInvitationsController::class, "index"])', RouteGenerator::fromPath($this->mapper, $path));
+        $this->assertContains('Route::post("/organizations/{uuid}/invitations", [CalendlyOrganizationInvitationsController::class, "create"])', RouteGenerator::fromPath($this->mapper, $path));
+    }
+
+    /** @test */
+    public function it_throws_exceptions()
+    {
+        $this->expectExceptionMessage('Could not determine a method for the path:');
+        $path = '/not/a/route';
+
+        RouteGenerator::fromPath($this->mapper, $path);
     }
 }
